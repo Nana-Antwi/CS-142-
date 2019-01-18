99d4a4ad0f4cec846effd47eae43789bf09686da
Nana Antwi <46608200+Nana-Antwi@users.noreply.github.com>
2019-01-18 15:02:57 -0500

    Add files via upload

---
+++ sitemap.php
@@ -0,0 +1,57 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Web development</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Nana Antwi">
+        <meta name="description" content="Web development(cs142)">
+    </head>
+
+    <body>
+
+        <h1>Nana Antwi</h1>
+
+        <h2>Web development</h2>
+        <h3>cs 142</h3>
+
+        <ol>
+            <li>Assignment 1: Style a form:
+                <a href="assignment1/form.php">Form Design</a>
+                -
+                <a href="assignment1/form.css">Style Sheet</a>
+                -
+                <a href="assignment1/form2.css">Alternate Style Sheet</a>
+            </li>
+            
+            <li>Assignment 2: Style a resume:
+                <a href="assignment2/resume.php">Resume Design</a>
+                -
+                <a href="assignment2/style.css">Style Sheet</a>
+                -
+                <a href="assignment2/style2.css">Alternate Style Sheet</a>
+            </li>
+            
+            <li>Assignment 3: Style a table:
+                <a href="assignment3/table.php">Table Design</a>
+                -
+                <a href="assignment3/table.css">Style Sheet</a>
+                -
+                <a href="assignment3/table2.css">Alternate Style Sheet</a>
+            </li>
+            
+            <li>Assignment 4: Style Course Page:
+                <a href="assignment4/nantwi.css">Style Sheet</a>
+            </li>
+        </ol>
+
+        <img src="pose_img.jpg" alt=" personal picture">
+
+
+    </body>
+
+</html>
